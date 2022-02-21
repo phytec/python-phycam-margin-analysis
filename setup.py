605c833da824ca8ca25c1eeadbcd645bbc15cee5
Stefan Riedmueller <s.riedmueller@phytec.de>
2022-02-21 11:41:59 +0100

    Switch to use static setup
    There is no need to have a dynamic setup script, thus change it to a
    static version and keep it backward compatible.
    
    Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>

--- setup.py
+++ setup.py
@@ -1,28 +1,5 @@
 #!/usr/bin/env python3
-
-from setuptools import setup
-
-def get_version():
-    return '1.0.0'
+import setuptools
 
 if __name__ == "__main__":
-    setup(
-        name='python-phycam-margin-analysis',
-        version=get_version(),
-        description='Python phyCAM MARGIN ANALYSIS',
-        packages=['src'],
-        author='PHYTEC Germany, Mainz',
-        author_email='b.feldmann@phytec.de',
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            #'License ::  :: ',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-        ],
-        python_requires='>=3.6',
-        install_requires=['smbus'],
-        entry_points={},
-        include_package_data=True,
-    )
+    setuptools.setup()
